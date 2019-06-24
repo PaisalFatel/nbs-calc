86162826d11ba708c3cd50ea9fe346756d781e98
PaisalFatel <faisalpatel7@gmail.com>
2019-06-24 15:11:57 +0100

    Added new feature

---
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.qa.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class CalculatorTest {
+
+	@Test
+	public void addTwoNumbersTogetherTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.add(2,2);
+		Assert.assertEquals(4,result);
+	}
+
+}
